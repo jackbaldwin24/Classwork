d52f96d0daaaedd8356479e40914b33f0d6c9c5e
Jack Baldwin <71354157+jackbaldwin24@users.noreply.github.com>
2021-12-27 13:24:48 -0500

    Delete CSE 331 directory

--- CSE 331/Project5/mytests.py
+++
@@ -1,4 +0,0 @@
-from PriorityHeap import Node, PriorityQueue, heap_sort, MaxHeap, find_ranking
-heap = PriorityQueue()
-heap.data = [Node(1, '231'), Node(2, '232'), Node(3, '260'), Node(4, '331'), Node(5, '431')]
-heap.get_parent_index(4)
